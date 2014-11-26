d558cb45c42709b164932e2810be3b0c43362eb0
Martin Bruce <mbr@e-conomic.com>
2014-11-26 12:39:47 +0100

    Added Sticky to H1 and H2 Added H3 to toc.jsAdded collaps on code examples.

--- lib/redcarpet_header_fix.rb
+++ lib/redcarpet_header_fix.rb
@@ -1,7 +1,13 @@
 module RedcarpetHeaderFix
   def header(text, level)
-    clean_id = text.downcase.gsub(/( +|\.+)/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
-    "<h#{level} id='#{clean_id}'>#{text}</h#{level}>"
+  	clean_id = text.downcase.gsub(/( +|\.+)/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
+  	if level = 1
+    	"<h#{level} id='#{clean_id} class="sticky"'>#{text}</h#{level}>"
+    else if level = 2
+    	"<h#{level} id='#{clean_id} class="sticky"'>#{text}</h#{level}>"
+	else
+		"<h#{level} id='#{clean_id}'>#{text}</h#{level}>"
+	end
   end
 end
 
