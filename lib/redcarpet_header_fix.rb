2b6477e05962ebf1c1381d365e26749c0cd74b6d
Martin Bruce <mbr@e-conomic.com>
2014-11-26 13:13:18 +0100

    Try 5 on sticky

--- lib/redcarpet_header_fix.rb
+++ lib/redcarpet_header_fix.rb
@@ -2,9 +2,9 @@ module RedcarpetHeaderFix
   def header(text, level)
   	clean_id = text.downcase.gsub(/( +|\.+)/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
   	if level == 1
-    	"<h#{level} id='#{clean_id} class="sticky"'>#{text}</h#{level}>"
+    	"<h#{level} id='#{clean_id}' class='sticky'>#{text}</h#{level}>"
     else if level == 2
-    	"<h#{level} id='#{clean_id} class="sticky"'>#{text}</h#{level}>"
+    	"<h#{level} id='#{clean_id}'class='sticky'>#{text}</h#{level}>"
 	else
 		"<h#{level} id='#{clean_id}'>#{text}</h#{level}>"
 	end
